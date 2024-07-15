d1326b27093410453618daf7c5c45147be41f731
HelenaGracia <160607290+HelenaGracia@users.noreply.github.com>
2024-07-15 11:06:28 +0200

    añadido imports necesarios de la parte animes

--- src/main/java/com/example/korner/controladores/AnimeController.java
+++ src/main/java/com/example/korner/controladores/AnimeController.java
@@ -1,6 +1,7 @@
 package com.example.korner.controladores;
 
 
+import com.example.korner.modelo.Animes;
 import com.example.korner.servicio.AnimeServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
