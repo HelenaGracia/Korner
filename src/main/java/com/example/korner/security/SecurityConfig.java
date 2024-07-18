ac8cd1702af0837d7aec59c45aea99cb25b58afa
carri1608 <carri1608@gmail.com>
2024-07-18 10:43:29 +0200

    *Cambiado endpoint de home

--- src/main/java/com/example/korner/security/SecurityConfig.java
+++ src/main/java/com/example/korner/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.formLogin(form -> form
                 .loginPage("/login")
-                .defaultSuccessUrl("/korner/home")
+                .defaultSuccessUrl("/home")
                 .permitAll()
         );
 
