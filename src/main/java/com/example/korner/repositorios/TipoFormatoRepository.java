45d322924950435defbf0376be25de2fcc4a1ba1
HelenaGracia <160607290+HelenaGracia@users.noreply.github.com>
2024-07-18 11:18:22 +0200

    añadidos archivos para igualar a Alvaro

---
+++ src/main/java/com/example/korner/repositorios/TipoFormatoRepository.java
@@ -0,0 +1,7 @@
+package com.example.korner.repositorios;
+
+import com.example.korner.modelo.TipoFormatos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipoFormatoRepository extends JpaRepository<TipoFormatos, Integer> {
+}
