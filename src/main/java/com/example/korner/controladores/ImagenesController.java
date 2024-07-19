8bd3662e31e8db122b5355b91753dcd1bef3d9e5
HelenaGracia <160607290+HelenaGracia@users.noreply.github.com>
2024-07-19 14:22:20 +0200

    añadidos archivos para subir imagenes: ImagenesController,FileSystemStorageService, StorageService. Pom y properties actualizado para imagenes. Modificado peliculaController y template pelicula para imagenes

---
+++ src/main/java/com/example/korner/controladores/ImagenesController.java
@@ -0,0 +1,44 @@
+package com.example.korner.controladores;
+
+import com.example.korner.servicio.FileSystemStorageService;
+
+import org.apache.catalina.util.IOTools;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+@RestController
+@RequestMapping("/imagenes")
+public class ImagenesController {
+
+
+    @GetMapping(value = "/leerImagen/{id}")
+    public ResponseEntity <byte[]> leerImagen(@PathVariable("id") String id) {
+        final String FILE_PATH_ROOT = "D:/ficheros";
+
+        byte[] image = new byte[0];
+        try {
+            image = FileUtils.readFileToByteArray(new File(FILE_PATH_ROOT+ "/"+ id));
+
+        }catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+//        if(FilenameUtils.getExtension(id).equals("jpg")){
+//            return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(image);
+//        }else {
+//            return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(image);
+//
+//        }
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(image);
+    }
+}
--- src/main/java/com/example/korner/controladores/PeliculaController.java
+++ src/main/java/com/example/korner/controladores/PeliculaController.java
@@ -4,14 +4,17 @@ import com.example.korner.modelo.GeneroElementoCompartido;
 import com.example.korner.modelo.Pelicula;
 import com.example.korner.repositorios.PeliculaRepository;
 
+import com.example.korner.servicio.FileSystemStorageService;
 import com.example.korner.servicio.GeneroElementoServiceImpl;
 import com.example.korner.servicio.PeliculaServiceImpl;
+import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -22,9 +25,12 @@ public class PeliculaController {
     PeliculaServiceImpl peliculaService;
     GeneroElementoServiceImpl generoElementoService;
 
-    public PeliculaController(PeliculaServiceImpl peliculaService, GeneroElementoServiceImpl generoElementoService) {
+    FileSystemStorageService fileSystemStorageService;
+
+    public PeliculaController(PeliculaServiceImpl peliculaService, GeneroElementoServiceImpl generoElementoService, FileSystemStorageService fileSystemStorageService) {
         this.peliculaService = peliculaService;
         this.generoElementoService = generoElementoService;
+        this.fileSystemStorageService = fileSystemStorageService;
     }
 
     private final Logger logger = LoggerFactory.getLogger(PeliculaController.class);
@@ -39,14 +45,21 @@ public class PeliculaController {
 
 
     @PostMapping("/save")
-    public String save(Pelicula pelicula){
-        logger.info("este es el objeto pelicula{}", pelicula);
+    public String save(@RequestParam("imagen") MultipartFile multipartFile, Pelicula pelicula){
+        logger.info("este es el objeto pelicula guardado{}", pelicula);
+        peliculaService.saveEntity(pelicula);
+        logger.info("este es el objeto pelicula guardado{}", pelicula);
+        String nombreArchivo = pelicula.getId() + "." + FilenameUtils.getExtension(multipartFile.getOriginalFilename());
+        fileSystemStorageService.storeWithName(multipartFile, nombreArchivo);
+        pelicula.setImagenRuta( "/imagenes/leerImagen/" + nombreArchivo);
         peliculaService.saveEntity(pelicula);
         return "redirect:/peliculas";
     }
 
+
     @PostMapping("/delete")
     public String delete(Pelicula pelicula){
+        logger.info("este es el objeto pelicula eliminado{}", pelicula);
         peliculaService.deleteEntity(pelicula);
         return "redirect:/peliculas";
     }
