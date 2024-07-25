9253f372780d621c74dd6d16e20dae50914c1e3f
HelenaGracia <160607290+HelenaGracia@users.noreply.github.com>
2024-07-25 20:07:01 +0200

    añadido validacion de Size para las opiniones (min 2, max 4000) y los mensajes de error personalizados para titulo y opinion que muestran los caracteres permitidos

--- src/main/java/com/example/korner/modelo/AbstractEntity.java
+++ src/main/java/com/example/korner/modelo/AbstractEntity.java
@@ -21,10 +21,10 @@ public abstract class AbstractEntity implements Serializable {
     @Column(name = "id", nullable = false)
     private Integer id;
 
-    @Column (name = "titulo" , length = 256, nullable = false)
+    @Column (name = "titulo" , length = 60, nullable = false)
     @NotBlank
-    @Size(min = 1)
-    @Size(max = 60)
+    @Size(min = 1, message = "Debe tener como mínimo 1 caracter")
+    @Size(max = 60,  message = "Debe tener como máximo 60 caracteres")
     private String titulo;
 
     @Column (name = "year")
@@ -39,10 +39,11 @@ public abstract class AbstractEntity implements Serializable {
 
     @Column (name = "opinion" , length = 4000)
     @NotBlank
+    @Size(min = 2, message = "Debe tener como mínimo 2 caracteres")
+    @Size(max = 4000, message = "Debe tener como máximo 4000 caracteres" )
     private String opinion;
 
     @Column (name = "imagen" , length = 4000)
-
     private String imagenRuta;
 
 
