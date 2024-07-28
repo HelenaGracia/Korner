7d353729865f9296b1bc8d99ebe4e0a9f04f285c
HelenaGracia <160607290+HelenaGracia@users.noreply.github.com>
2024-07-28 20:06:53 +0200

    Implementada paginacion en peliculas pero genera fallo cuando se generan errores de vaidacion o se intenta modificar la pelicula

--- src/main/java/com/example/korner/controladores/PeliculaController.java
+++ src/main/java/com/example/korner/controladores/PeliculaController.java
@@ -14,6 +14,8 @@ import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -28,6 +30,9 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/peliculas")
@@ -51,15 +56,47 @@ public class PeliculaController {
 
     //Mostrar Peliculas
     @GetMapping("")
-    public String listAllPeliculas(Model model){
+    public String listAllPeliculas(Model model, @RequestParam("page") Optional<Integer> page){
+
+        //Recibe la pagina en la que estoy si no recibe nada asigna la pagina 1
+        int currentPage = page.orElse(1);
+        //Guarda la pagina en la que estoy (Si es la pagina 1, la 2...) y la cantidad de elementos que quiero mostrar en ella
+        PageRequest pageRequest = PageRequest.of(currentPage-1, 2);
+        /*
+         se crea un objeto page que es el encargado de rellenar en la pagina que le has indicado con la cantidad
+         que le has dicho todos los objetos pelicula almacenados, es decir, crea la pagina que visualizas con el contenido
+         */
+        Page<Pelicula> pagina = peliculaService.findAll(pageRequest);
+        //Envio la pagina creada a la vista para poder verla
+        model.addAttribute("pagina", pagina);
+        //Obtengo la cantidad de paginas creadas, por ejemplo: 8
+        int totalPages = pagina.getTotalPages();
+
+        /*
+         Si la cantidad total de paginas es superior a 0 obtiene una lista con los numeros de pagina, es decir
+         si tengo un total de 8 paginas va a crear una lista de Integer almacenando los valores 1,2,3,4,5,6,7,8
+         de esta forma obtego todos los numeros de pagina y los envio a la vista
+         */
+        if (totalPages > 0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
+        //Envio a la vista la pagina en la que estoy
+        model.addAttribute("currentPage", currentPage);
+
+
         List<Pelicula> listadoPeliculas = peliculaService.getAll();
         Pelicula pelicula = new Pelicula();
         List<GeneroElementoCompartido> generoElementoCompartidoList = generoElementoService.getAll();
         List<Plataforma> plataformasList = plataformaService.getAll();
         model.addAttribute("listaGeneros", generoElementoCompartidoList);
         model.addAttribute("listaPlataformas", plataformasList);
-        model.addAttribute("size", listadoPeliculas.size());
-        model.addAttribute("peliculas", listadoPeliculas);
+        //model.addAttribute("size", listadoPeliculas.size());
+        model.addAttribute("size", pagina.getContent().size());
+        //model.addAttribute("peliculas", listadoPeliculas);
+        model.addAttribute("peliculas", pagina.getContent());
         model.addAttribute("datosPelicula", pelicula);
         return "peliculas";
     }
@@ -197,6 +234,8 @@ public class PeliculaController {
 
         return "redirect:/peliculas";
     }
+
+
 
 
 }
