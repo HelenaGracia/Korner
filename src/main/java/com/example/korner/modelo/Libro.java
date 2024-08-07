94fc8bc9747b0dc8e5f3728a17bf493ce617147c
Irene <irene2rod@gmail.com>
2024-08-07 16:46:18 +0200

    Actualización de Repositorio, Servicio, Controlador, Modelo y HTML de Libro. Añadido tipoFormatos.

--- src/main/java/com/example/korner/modelo/Libro.java
+++ src/main/java/com/example/korner/modelo/Libro.java
@@ -1,35 +1,48 @@
 package com.example.korner.modelo;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
 
-import java.util.Date;
+import java.util.Set;
 
-@Entity
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Table(name = "libros",
+@Entity
+@Table(name = "libros" ,uniqueConstraints = @UniqueConstraint(columnNames = "titulo"),
         indexes = {@Index(name = "indice1",columnList = "titulo"),
                 @Index(name = "indice2",columnList = "year"),
-                @Index(name = "indice3",columnList = "id_generos_elemt_comp")})
-public class Libro extends AbstractEntity{
+                @Index(name = "indice3",columnList = "puntuacion")})
+public class Libro  extends AbstractEntity{
+
+    @ManyToMany (fetch = FetchType.LAZY)
+    @JoinTable(name = "libro_plataforma", joinColumns = @JoinColumn(name = "id"),
+            inverseJoinColumns = @JoinColumn(name = "id_plataforma"))
+    @NotEmpty
+    private Set<Plataforma> plataformasLibro;
 
 
-    @ManyToOne (fetch = FetchType.EAGER)
-    @JoinColumn(name = "id_generos_elemt_comp", foreignKey = @ForeignKey(name = "fk_generos_elemt_comp_libro"))
-    private GeneroElementoCompartido generoElementoCompartidoLibro;
 
-    @ManyToOne (fetch = FetchType.EAGER)
-    @JoinColumn(name = "id_plataformas", foreignKey = @ForeignKey(name = "fk_plataformas_libro"))
-    private Plataforma plataformasLibro;
+    @ManyToMany (fetch = FetchType.LAZY)
+    @JoinTable(name = "libro_genero", joinColumns = @JoinColumn(name = "id"),
+            inverseJoinColumns = @JoinColumn(name = "id_generos_elemt_comp"))
+    @NotEmpty
+    private Set<GeneroElementoCompartido> generosLibro;
 
-    @ManyToOne (fetch = FetchType.EAGER)
-    @JoinColumn(name = "id_tipos_formatos", foreignKey = @ForeignKey(name = "fk_tipos_formatos_libro"))
+    @ManyToOne (fetch = FetchType.LAZY)
+    @JoinColumn (name = "id_libro_usuarios")
+    private Usuario usuarioLibro;
+    
+    @ManyToOne (fetch = FetchType.LAZY)
+    @JoinColumn (name = "id_tipo_formatos")
     private TipoFormatos tipoFormatos;
 
+    @Override
+    public String toString() {
+        return "Libro{" +
+                ", plataformasLibro=" + plataformasLibro +
+                ", generoElementoCompartidoLibro=" + generosLibro +
+                '}'+ super.toString();
+    }
 }
